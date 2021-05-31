d9b428ec36d9482f9f476435ab61d3b65fce93aa
Reynold Osei Adade <reynold@walulel.com>
2021-05-31 11:01:17 +0000

    setup header

---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-full p-4 flex justify-around bg-red-100">
+    <div v-for="items in header" :key="items" class="uppercase font-semibold">
+      {{ items }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      header: ["introduction", "skills", "projects", "hobbies", "socials"]
+    };
+  }
+};
+</script>
+
+<style></style>
