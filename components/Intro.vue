fe25ecb87c9eac5b52eb446716d5692f5e6ee415
Reynold Osei Adade <reynold@walulel.com>
2021-05-31 11:51:20 +0000

    setup intro

---
+++ components/Intro.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="w-full grid grid-cols-1 md:grid-cols-2">
+    <div>
+      <lottie-player
+        src="https://assets6.lottiefiles.com/packages/lf20_5adtCi.json"
+        background="transparent"
+        speed="1"
+        style="width: 100%; height: 100%;"
+        loop
+        autoplay
+      ></lottie-player>
+    </div>
+    <div class="text-center flex items-center justify-center">
+      <div class="py-4 px-8 text-sm">
+        Hi, my name is Reynold, welcome to my portfolio, where i get to talk
+        about myself, at least just in the introduction, I have been programming
+        professionally, meaning someone actually thought it a good idea to pay
+        me to write code, since 2015, so come with me while i take you through
+        the projects I've worked since then, I hope you appreciate my journey as
+        I have
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="w-full">
     <Header />
+    <Intro />
   </div>
 </template>
 
 <script>
 import Header from "~/components/Header";
+import Intro from "~/components/Intro";
 export default {
   components: {
-    Header
+    Header,
+    Intro
   }
 };
 </script>
