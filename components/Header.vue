b706307878dd273b14f29f4afed75588dca51efa
reynoldadade <reynoldadade@gmail.com>
2021-08-31 06:34:09 +0000

    updated look

--- components/Header.vue
+++ components/Header.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class=" m-2 p-4 text-sm">
-    <div class="text-center text-5xl px-2 py-8 uppercase font-bold text-white">
+  <div class=" m-2 p-4 text-sm text-white'">
+    <div class="text-center text-5xl px-2 py-8 uppercase font-bold ">
       Other interesting facts
     </div>
     <div class="grid grid-cols-4 gap-6">
-      <div class="bg-white p-1 shadow ">
+      <div class=" p-1 shadow text-white">
         <p>
           I am planning to build a unified healthcare management system, that
           allows hospital to provide part or all their services to patients
         </p>
       </div>
-      <div class="bg-white p-1 shadow ">
+      <div class=" p-1 shadow text-white">
         <p>
           I believe there is no joy in a job that does not challenge you,
           because there isn't a single reward it can give since there aren't
