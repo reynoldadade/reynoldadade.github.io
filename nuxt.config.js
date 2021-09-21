c1a5337f9ba1fc4d31fc8430eb340bc09201c727
reynoldadade <reynoldadade@gmail.com>
2021-09-21 22:28:50 +0000

    added description tag to page

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,11 @@ export default {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+      {
+        hid: "description",
+        name: "description",
+        content: "Reynold Adade's porfolio page"
+      }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     script: [
